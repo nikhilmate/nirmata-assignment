fe0b13c6c067fcc33b555c353dbe96d6d351fcb5
nikhilmate2112@gmail.com <nikhil.mate@samespace.com>
2023-06-30 15:24:00 +0530

    feature: added close icon in searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,17 +1,31 @@
 import { EActions, IState, TAction } from "../utils/types"
 import Container from "./Container"
+import SearchIcon from '../assets/icons/search-icon.svg'
+import CloseIcon from '../assets/icons/close-icon.svg'
+import React from "react"
 
 function SearchBar({ filters, setFilter }: { filters: IState, setFilter: React.Dispatch<TAction> }) {
+  
+  const changeInput = (value: string) => setFilter({ type: EActions.setStore, payload: Object.assign(filters, { search: value })})
+  
   return (
     <header className="h-12 sticky left-0 top-14 shadow-[0px_2px_4px_rgba(0,0,0,0.06)] z-20">
-      <Container className="flex h-full">
-        <input
-          type="text"
-          placeholder="Search by name and type"
-          className="w-full h-full text-base font-normal text-gray-800 placeholder:text-gray-600 placeholder:font-light focus:outline-none"
-          value={filters.search}
-          onChange={(ev) => setFilter({ type: EActions.setStore, payload: Object.assign(filters, { search: ev.target.value })})}
-        />
+      <Container className="flex items-stretch h-full gap-x-2">
+        <span
+          className={`inline-flex items-center ${!filters.search ? 'pointer-events-none' : 'cursor-pointer'}`}
+          onMouseDown={() => changeInput('')}
+        >
+          <img className="w-4 h-4" src={filters.search ? CloseIcon : SearchIcon} alt="" />
+        </span>
+        <span>
+          <input
+            type="text"
+            placeholder="Search by name and type"
+            className="w-full h-full text-base font-normal text-gray-800 placeholder:text-gray-600 placeholder:font-light focus:outline-none"
+            value={filters.search}
+            onChange={(ev: React.ChangeEvent<HTMLInputElement>) => changeInput(ev.target.value)}
+          />
+        </span>
       </Container>
     </header>
   )
